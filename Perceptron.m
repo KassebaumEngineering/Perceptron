3859e65ce8cb9b3761ca9493b8c14cc11e3edfdc
jak <jak>
1998-03-04 06:51:07 +0000

    Added a Nguyen-Widrow initialization and compiled minimization sum. -jak

--- Perceptron.m
+++ Perceptron.m
@@ -14,7 +14,7 @@
  
 (* :Context: Classes`Perceptron` *)
 
-(* :Package Version: 1.0 - $Id: Perceptron.m,v 1.10 1998/02/27 04:09:59 jak Exp $ *)
+(* :Package Version: 1.0 - $Id: Perceptron.m,v 1.11 1998/03/04 06:51:07 jak Exp $ *)
 
 (* :Mathematica Version: 3.0 *)
 
@@ -39,7 +39,21 @@ BeginPackage[ "Classes`Perceptron`",
 new::usage  = "new[Perceptron, numOfInputs, numOfHiddenUnits, numOfOutputs ] 
             generates a new instance of the Perceptron class with the 
 			given architecture."
+Initialize::usage  = "Initialize[ APerceptronInstance, inputSamples, DesiredOutputSamples ] 
+            initializes the perceptron instance given a data collection, and returns the
+			Perceptron Instance.  This speeds convergence damatically."
 Print::usage  = "Print[ APerceptronInstance ] prints facts about the Instance."
+SetHiddenWeights::usage  = "SetHiddenWeights[ APerceptronInstance, Values ] sets the 
+            Hidden Weight Matrix for the Perceptron Instance."
+SetHiddenBiases::usage  = "SetHiddenBiases[ APerceptronInstance, Values] sets the 
+            Hidden Bias Matrix for the Perceptron Instance."
+SetOutputWeights::usage  = "SetOutputWeights[ APerceptronInstance, Values] sets the 
+            Output Weight Matrix for the Perceptron Instance."
+SetOutputBiases::usage  = "SetOutputBiases[ APerceptronInstance, Values] sets the 
+            Output Bias Matrix for the Perceptron Instance."
+SetParameters::usage  = "SetParameters[ APerceptronInstance, Values] sets a list of 
+            Parameter Matrices for the Perceptron Instance, in the order
+			of Hidden Weights, Hidden Biases, Output Weights, Output Biases."
 GetHiddenWeights::usage  = "GetHiddenWeights[ APerceptronInstance ] returns the 
             Hidden Weight Matrix for the Perceptron Instance."
 GetHiddenBiases::usage  = "GetHiddenBiases[ APerceptronInstance ] returns the 
@@ -91,23 +105,24 @@ Class[
         OutputFn,
         ErrorFn,
         GradientFn,
+		InputSyms,
+		OutputSyms,
+		HiddenUnitSyms,
+		hiddenWeights,
+		hiddenBiases,
+		outputWeights,
+		outputBiases,
+		ParamSymbols,
+		errorF,
+		hiddenF,
+		outputF,
+		errFn,
+		HDtable
     },
     {    {  new,  (* numOfInputs, numOfHiddenUnits, numOfOutputs *)
             Function[ 
 			  Module[ 
 			    { 
-				  InputSyms,
-				  OutputSyms,
-				  HiddenUnitSyms,
-				  hiddenWeights,
-				  hiddenBiases,
-				  outputWeights,
-				  outputBiases,
-				  ParamSymbols,
-				  hiddenF,
-				  ouputF,
-				  errorF,
-				  HDTable,
 				  DsseDhiddenUnits,
 				  DsseDoutputWeights,
 				  DsseDoutputBiases,
@@ -143,7 +158,7 @@ Class[
 			    outputBiases  = Array[ Unique[outbs]&, {1, numOfOutputs}               ];
 				ParamSymbols  = Flatten[{hiddenWeights, hiddenBiases, outputWeights, outputBiases}];
 				
-            (* Intrinsic Functions *)
+             (* Intrinsic Functions *)
                 hiddenF = Tanh[ InputSyms . hiddenWeights + hiddenBiases ];
                 outputF  = HiddenUnitSyms . outputWeights + outputBiases;
                 errorF  = (OutputSyms - outputF) . Transpose[(OutputSyms - outputF)];
@@ -152,7 +167,7 @@ Class[
                     Table[ HiddenUnitSyms[[1,i]] -> hiddenF[[1,i]], {i,numOfHiddenUnits}]
 				]];
 				
-            (* Compile Network Output Function *)
+             (* Compile Network Output Function *)
 				OutputFn = Compile[ 
 				    Release[ Flatten[{ InputSyms, ParamSymbols }]],
 					Release[ 
@@ -162,7 +177,7 @@ Class[
 					]
 				];
 				
-            (* Compile Network Error Function *)
+             (* Compile Network Error Function *)
 				ErrorFn = Compile[ 
 				    Release[ Flatten[{ InputSyms, OutputSyms, ParamSymbols }]],
 					Release[ 
@@ -171,8 +186,16 @@ Class[
 						]
 					]
 				];
+				errFn = Function[
+					Release[ Flatten[{ InputSyms, OutputSyms, ParamSymbols }]],
+					Release[ 
+						Return[
+							(errorF /. HDtable)
+						]
+					]
+				];
 					
-            (* Error Gradient Function Expressions *)
+             (* Error Gradient Function Expressions *)
                 DsseDhiddenUnits           = Table[ D[         errorF, HiddenUnitSyms[[1,i]] ],{i,numOfHiddenUnits}                 ];
                 DsseDoutputWeights         = Table[ D[         errorF,  outputWeights[[i,j]] ],{i,numOfHiddenUnits},{j,numOfOutputs}];
                 DsseDoutputBiases          ={Table[ D[         errorF,   outputBiases[[1,i]] ],{i, numOfOutputs}                    ]};
@@ -182,7 +205,7 @@ Class[
                 DsseDhiddenWeights         = Table[ DsseDhiddenUnits[[j]] DhiddenUnitsDhiddenWeights[[i,j]],{i,numOfInputs},{j,numOfHiddenUnits}];
                 DsseDhiddenBiases          ={Table[ DsseDhiddenUnits[[j]] DhiddenUnitsDhiddenBiases[[j]]   ,{j,numOfHiddenUnits}                ]};
 					
-            (* Compile Network Gradient Function *)
+             (* Compile Network Gradient Function *)
 				GradientFn = List[
 					Compile[ Release[ Flatten[{ InputSyms, OutputSyms, ParamSymbols }]],
 						Release[ 
@@ -193,7 +216,7 @@ Class[
 					],
 					Compile[ Release[ Flatten[{ InputSyms, OutputSyms, ParamSymbols }]],
 						Release[ 
-							Return[
+						    Return[
 							    Flatten[Transpose[( DsseDhiddenBiases /. HDtable),{3,4,1,2}],2]
 							]
 						]
@@ -214,15 +237,85 @@ Class[
 					]
 				];
 				
-			(* Initialize to random starting parameter values *)
-                hidWtVals = Array[ Random[ Statistics`NormalDistribution`NormalDistribution[0,0.1] ]&, {numOfInputs, numOfHiddenUnits}];
-                hidBsVals = Array[ Random[ Statistics`NormalDistribution`NormalDistribution[0,0.1] ]&, {1, numOfHiddenUnits}          ];
-                outWtVals = Array[ Random[ Statistics`NormalDistribution`NormalDistribution[0,0.1] ]&, {numOfHiddenUnits,numOfOutputs}];
-                outBsVals = Array[ Random[ Statistics`NormalDistribution`NormalDistribution[0,0.1] ]&, {1, numOfOutputs}              ];
+			 (* Initialize to Nguyen-Widrow starting parameter values *)
+                hidWtVals = Array[ Random[ Real, {-1.0, 1.0}]&, {numOfInputs, numOfHiddenUnits}];
+                hidBsVals = Array[ Random[ Real, {-1.0, 1.0}]&, {1, numOfHiddenUnits}          ];
+                outWtVals = Array[ Random[ Real, {-1.0, 1.0}]&, {numOfHiddenUnits,numOfOutputs}];
+                outBsVals = Array[ Random[ Real, {-1.0, 1.0}]&, {1, numOfOutputs}              ];
+				    
+			 (* Normalize Rows of Hidden Weight Matrix *)
+				For[ i=1, i<=numOfInputs, i++,
+				    hidWtVals[[i]] = hidWtVals[[i]] * ( 1.0 / Sqrt[ hidWtVals[[i]] . hidWtVals[[i]]] );
+				];
+				    
+			 (* Scale to meet hidden layer size *)
+			    magfactor = 0.7 * numOfHiddenUnits^(1/numOfInputs);
+			    hidWtVals = magfactor * hidWtVals;
+			    hidBsVals = magfactor * hidBsVals;
+
+			 (* Set the Instance variable *)
 				Params = {hidWtVals, hidBsVals, outWtVals, outBsVals};
 			  ]
 			] 
          },
+		 { Initialize, (* initialize the parameter matrices for a given data set *)
+		   Function[  {inputSamples,outputSamples},
+		        Module[ {hidWtVals, hidBsVals, outWtVals, outBsVals, magfactor, i, scalefactors, biasfactors },
+				 (* Initialize to Nguyen-Widrow  starting parameter values *)
+					hidWtVals = Array[ Random[ Real, {-1.0, 1.0}]&, {numOfInputs, numOfHiddenUnits}];
+					hidBsVals = Array[ Random[ Real, {-1.0, 1.0}]&, {1, numOfHiddenUnits}          ];
+					outWtVals = Array[ Random[ Real, {-1.0, 1.0}]&, {numOfHiddenUnits,numOfOutputs}];
+					outBsVals = Array[ Random[ Real, {-1.0, 1.0}]&, {1, numOfOutputs}              ];
+				    
+				 (* Normalize Rows of Hidden Weight Matrix *)
+				    For[ i=1, i<=numOfInputs, i++,
+				        hidWtVals[[i]] = hidWtVals[[i]] * ( 1.0 / (Sqrt[ hidWtVals[[i]] . hidWtVals[[i]]]) );
+				    ];
+				    
+				 (* Scale to meet hidden layer size *)
+				    magfactor = 0.7 * numOfHiddenUnits^(1/numOfInputs);
+				    hidWtVals = magfactor * hidWtVals;
+				    hidBsVals = magfactor * hidBsVals;
+			    
+				 (* Scale to meet input and output samples *)
+				    scalefactors = Table[
+				        ( Max[ Transpose[ inputSamples ][[i]] ]
+				        - Min[ Transpose[ inputSamples ][[i]] ]),
+				        {i, numOfInputs }
+				    ];
+				    biasfactors = Table[
+				        ( Max[ Transpose[ inputSamples ][[i]] ]
+				        + Min[ Transpose[ inputSamples ][[i]] ]) / 2,
+				        {i, numOfInputs }
+				    ];
+					For[ i=1, i<=numOfInputs, i++,
+					    hidWtVals[[i]] = 2 * hidWtVals[[i]]  / scalefactors[[i]];
+					];
+					hidBsVals = hidBsVals - {biasfactors}.hidWtVals;
+					
+				 (* Pre select the Output Layer to minimize error *)
+				    Module[ {hlayer, Wy, H, Y},
+                        hlayer = Transpose[ Append[
+						    Transpose[ Tanh[ inputSamples . hidWtVals + Array[ (hidBsVals[[1]])&, {Length[inputSamples]}] ]],
+							Array[ (1)&, {Length[inputSamples]}]
+						]];
+						H = Transpose[hlayer] . hlayer;
+						Y = Transpose[hlayer] . outputSamples;
+                        Wy = Dot[ 
+                            Inverse[H], 
+							Y
+                        ]; 
+                        outBsVals = { Wy[[numOfHiddenUnits + 1]] };
+                        outWtVals = Delete[ Wy , numOfHiddenUnits + 1 ];
+                    ];
+                    
+				 (* Set Instance Variable *)
+					Params = {hidWtVals, hidBsVals, outWtVals, outBsVals};
+					
+					Return[ self ];
+				]
+		   ]
+		 },
          { Print, (* no arguments *)
            Function[
                 Print["numOfInputs:      ", numOfInputs      ];
@@ -246,8 +339,33 @@ Class[
 		 { GetOutputBiases, (* return output bias matrix *)
 		   (Params[[4]])&
 		 },
-		 { GetParameters, (* no arguments *)
+		 { GetParameters, (* return list of all parameter matrices *)
 		   Params&
+		 },
+		 { SetHiddenWeights, (* SET hidden weight matrix *)
+           Function[ newHiddenWeights,
+		       Params[[1]] = newHiddenWeights; 
+		   ]
+		 },
+		 { SetHiddenBiases, (* SET hidden bias matrix *)
+           Function[ newHiddenBiases,
+		       Params[[2]] = newHiddenBiases; 
+		   ]
+		 },
+		 { SetOutputWeights, (* SET output weight matrix*)
+           Function[ newOutputWeights,
+		       Params[[3]] = newOutputWeights; 
+		   ]
+		 },
+		 { SetOutputBiases, (* SET output bias matrix *)
+           Function[ newOutputBiases,
+		       Params[[4]] = newOutputBiases; 
+		   ]
+		 },
+		 { SetParameters, (* SET all parameter matrices *)
+           Function[ newParams,
+		       Params = newParams; 
+		   ]
 		 },
          { N,
            Function[ inputSamples,
@@ -289,22 +407,22 @@ Class[
            GradientFn&
          },
          { ErrorGradient,  (* Weight Gradient *)
-           Function[ {inputSamples, outputSamples},
-               Module[ {i, j},
-                  (* Calculate and Return Gradient Sum for all input/output samples *)
-                    Return[
-                        Sum[ 
-                            Table[ 
-                                Apply[ 
-							        GradientFn[[i]], 
-								    Flatten[{inputSamples[[j]], outputSamples[[j]], Params}]
-							    ],
-                                {i, Length[GradientFn] }
-                            ],
-                            {j, Length[ outputSamples]}
-                        ]
-                    ]
-                ]
+		   Function[ {inputSamples, outputSamples},
+				Module[ {i, j},
+					(* Calculate and Return Gradient Sum for all input/output samples *)
+					Return[
+						Sum[ 
+							Table[ 
+								Apply[ 
+									GradientFn[[i]], 
+									Flatten[{inputSamples[[j]], outputSamples[[j]], Params}]
+								],
+								{i, Length[GradientFn] }
+							],
+							{j, Length[ outputSamples]}
+						]
+					]
+				]
            ]
          },
          { Train,  (* Via Conjugate Gradient Descent *)
@@ -335,15 +453,17 @@ Class[
                        W0 =. ; W0 = W1;
                        
                      (* Choose Learning Rate *)
-                       ErrMin = Module[ {f, j},
-                            f = Function[ x,
-                                Sum[
-                                    Apply[ 
-                                        ErrorFn,
-                                        Flatten[ {inputSamples[[j]], outputSamples[[j]], (W0 + x P0)} ]
-                                    ][[1]],
-                                    {j, Length[outputSamples]}
-                                ][[1]]
+                       ErrMin = Module[ {f,j, x},
+                            f = Compile[ {x},
+							    Release[
+									Sum[
+										Apply[ 
+											errFn,
+											Flatten[ { inputSamples[[j]], outputSamples[[j]], (W0 + x P0) } ]
+										][[1]],
+										{j, Length[outputSamples]}
+									][[1,1]]
+								]
                             ];
                             FindMinimum[ f[ LearningRate ], {LearningRate, {0.1, 0.2}}]
                        ];
@@ -399,6 +519,9 @@ EndPackage[]
 
 (* :History:
    $Log: Perceptron.m,v $
+   Revision 1.11  1998/03/04 06:51:07  jak
+   Added a Nguyen-Widrow initialization and compiled minimization sum. -jak
+
    Revision 1.10  1998/02/27 04:09:59  jak
    A Major Re-write - maybe it'll converge now. -jak
 
